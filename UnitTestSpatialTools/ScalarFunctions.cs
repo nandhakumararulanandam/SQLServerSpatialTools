730dd72c60021b17aa3bd615c41809d326a817b2
Jeyaraman-Esakki <41890650+Jeyaraman-Esakki@users.noreply.github.com>
2019-02-07 19:25:18 +0530

    Updated Test cases
    Updated the initial level of test cases for methods like MergeGeometrySegments,
    PopulateGeometryMeasures

--- UnitTestSpatialTools/ScalarFunctions.cs
+++ UnitTestSpatialTools/ScalarFunctions.cs
@@ -31,5 +31,38 @@ namespace SpatialTools.UnitTest
             var sqlgeom = Functions.InterpolateBetweenGeom(geom1, geom2, distance);
             Assert.IsTrue((bool)sqlgeom.STEquals(returnPoint));
         }
+
+        [TestMethod]
+        public static void PopulateGeometryMeasures_Test()
+        {
+            var geom = SqlGeometry.STLineFromText(new SqlChars("LINESTRING (10 1 10 100, 15 5 10 150 )"), Functions.DEFAULT_SRID); //
+            var populatedGeometry = Functions.PopulateGeometryMeasures(geom, null, null);
+            // if the start, end measure would be null, then this function populates the existing 'M' (measure) value
+            Assert.IsTrue((bool)(populatedGeometry.STPointN(1).M == 100));
+            Assert.IsTrue((bool)(populatedGeometry.STPointN(1).M == 150));
+            double updatedStartMeasure = 200;
+            double updatedEndMeasure = 200;
+            // if the start, end measure would be non null, then this function overrides the 'M' value that has been passed
+            populatedGeometry = Functions.PopulateGeometryMeasures(geom, updatedStartMeasure, updatedEndMeasure);
+            Assert.IsTrue((bool)(populatedGeometry.STPointN(1).M == updatedStartMeasure));
+            Assert.IsTrue((bool)(populatedGeometry.STPointN(1).M == updatedEndMeasure));
+        }
+  
+        [TestMethod]
+        public static void MergeGeometrySegments_Test()
+        { 
+            var geom1 = "LINESTRING(1 1 NULL 0, 1 5 NULL 1)".GetGeom();
+            var geom2 = "LINESTRING(1 5 NULL 5, 2 3 NULL 0)".GetGeom();
+            var resultantGeom = Functions.MergeGeometrySegments(geom1, geom2);
+            var lastPoint = resultantGeom.STPointN(Int32.Parse(resultantGeom.STNumPoints().ToString()));
+            Assert.IsTrue((bool)(lastPoint.STStartPoint().STX == 2));   // should be the last point's x value
+            Assert.IsTrue((bool)(lastPoint.STStartPoint().STY == 3));   // should be the last point's y value
+            /*
+                TODO: Have to implement polygon with various co-ordinates
+                geom1 = "POLYGON((3 3 , 3 6 , 6 6 , 6 3 , 3 3 ))".GetGeom();
+                geom2 = "POLYGON((2 2 , 2 8 , 8 8 , 2 8 , 2 2 ))".GetGeom();
+                resultantGeom = Functions.MergeGeometrySegments(geom1, geom2);
+            */
+        }
     }
 }
