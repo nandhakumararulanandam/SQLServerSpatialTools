5803f30ef14b537d3fa8ab3ce4a70cccc865052d
Nandha Kumar A <nandhak@virtuositycg.com>
2019-05-28 09:58:58 +0530

    Updated assembly versions for Sprint 5 release

--- unittest/Properties/AssemblyInfo.cs
+++ unittest/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("5fcdb549-1b10-4115-9a3f-aa55584fea23")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
